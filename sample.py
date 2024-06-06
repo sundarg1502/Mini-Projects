ea3f0004551c822dc940c463aef5f1fba0722fd4
sundarg1502 <sundarg1502@gmail.com>
2024-06-06 07:36:19 +0530

    FEET : Added a txt file

--- sample.py
+++ sample.py
@@ -1,1 +1,7 @@
-print("Sample code for GITHUB")
+print("Sample code for github")  # Check conventional commits website
+'''
+to add a file git -> git add .\filename.extension
+to commit -> git commit -> it commits the added files 
+git commit -m "message for the file visit conventional commits site for more"
+git push orgin remote     orgin-> git cloned link
+'''
